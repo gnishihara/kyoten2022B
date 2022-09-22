d5452f48e9f4e1d2cf7833dc723036e8cc822859
Greg Nishihara <gnishihara@gmail.com>
2022-09-22 12:46:53 +0900

    Update

---
+++ kyoten2022B-day02-problem02.R
@@ -0,0 +1,40 @@
+####################################################
+# 公開臨海データ解析実習　B日程
+# 2022 September 21 PM
+# Greg Nishihara
+####################################################
+# パッケージの読み込み
+library(tidyverse) # データ処理
+library(lubridate) # 時系列データ
+library(emmeans)   # 多重比較
+library(car)       # Type-II ANOVA
+library(statmod)   # qresiduals (ダンスミス残渣・ランダム化残渣)
+library(ggpubr)　　# theme_pubr()
+library(patchwork) # 複数図の結合
+library(magick)    # pdf を png に変換する
+library(showtext)  # フォント埋め込み用
+################################################################
+# font_files() |> as_tibble() # システムフォントの閲覧
+# font_files() |> as_tibble() |>
+# # filter(str_detect(ps_name, "NotoSansCJK")) |> 
+# select(file, face, ps_name) 
+# 埋め込みフォントの指定
+font_add(family = "notosansjp",
+         regular = "NotoSansCJKjp-Regular.otf")
+# フォントを有効にする
+theme_gray(base_family = "notosansjp") |> theme_set()
+# Windows の場合
+# font_add(family = "meiryo", regular = "meiryo.ttc")
+# theme_gray(base_family = "meiryo") |> theme_set()
+showtext_auto()
+
+################################################################
+
+data(mammalsleep, package = "faraway")
+
+# 説明変数：brain, lifespan, gestation, sleep, dream
+# 応答変数：body
+
+
+# 一般化線形モデルを当てはめ、診断図と解析結果を示す。
+
